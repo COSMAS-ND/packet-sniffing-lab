68e2ce497fa1b5fa5f4b3de76d559d314437504d
COSMAS-ND <osmas.nnadube@gmail.com>
2026-05-28 14:57:19 +0100

    Update secure.py

--- secure.py
+++ secure.py
@@ -4,5 +4,5 @@ conn = sqlite3.connect(":memory:")
 cursor = conn.cursor()
 
 username = input("Enter username: ")
-cursor.execute("SELECT ? ", (username,))
+cursor.execute("SELECT * FROM users WHERE username = ?" , (username, ))
 print("Query executed safely")
